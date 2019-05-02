bd247deba46d49152bd864f529d29eb9e0e1ce1a
temptgithub <2talke2tempt@gmail.com>
2019-05-02 16:18:41 -0700

    changes to index.js file to put objects into database

--- index.js
+++ index.js
@@ -1,149 +1,116 @@
-const express = require("express");    // this is all you need for a server
-const app = express();    // variable run as a function
-const bodyParser = require("body-parser");
-const fs = require("fs");
+const express = require("express");
 const mongoose = require("mongoose");
 
+const app = express();
+const http = require("http").Server(app);
+const bodyParser = require("body-parser");
 
-
-let http = require("http").Server(app);  //class with a Server Method that accepts express server as variable intake
-
-app.use("/", express.static("./client") ); // serving the files on client-this is in reference to the html?
-// app.use("/secretfolder", express.static("private/") );   // setting up different static directory roots -  routing to different folder
-// following two lines are for body parser to understand objects thru post requests
+app.use("/", express.static("./client"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:false}));
 
-
-const port =3000;
+const port = 3000;
 const connection = "mongodb+srv://nodeuser:123456nodeuser@cluster0-oc6sg.mongodb.net/test?retryWrites=true";
-
-app.listen(port);
-
-/*
-const MongoClient = require('mongodb').MongoClient;
-const uri = "mongodb+srv://practiceUser:<password>@mongopractice-matuh.mongodb.net/test?retryWrites=true";
-const client = new MongoClient(uri, { useNewUrlParser: true });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
-*/
-
-
-
+http.listen(port);
+console.log("Express is now running.");
 
 mongoose.connect(connection, (error) => {
-  if (error) {
-      console.log(`Failed to connect to database ${error}`);
-  } else {
-      console.log(`Successfully connected to database`);
+	if (error) {
+		console.log("There was an error with Mongoose", error);
+	} else {
+		console.log("Connection successful");
+	}
+});
 
-  }
+// Copies JS Promise to mongoose.
+mongoose.Promise = global.Promise;
 
-});             // connects to internet
+let db = mongoose.connection;
 
-mongoose.Promise = global.Promise;  // js built in class to understand what promise is
-//copies js promise to Mongoose
-
-let db = mongoose.connection;  // defining the database as a variable
-// turning on the database connection, bind connects btwn objects eg console w/ err txt msgs
-//on and connect are mongol methods
+// Tells mongoose what to do with MongoDB errors and also tells it to send it to the JS console.
 db.on('error', console.error.bind(console, "MongoDB connection error: "));
-//nosql how to save an objects uses js class to organize things
-// grabs a copy of the empty Mongoose package class
 
-
-//grabs copy of message schema class
-
+// Grabs a copy of the empty Mongoose Schema class.
 let Schema = mongoose.Schema;
-// customizes our empty class innto a custom class and stored i mySchema.
-// making columns for rows
 
-// customizes our empty class into a custom class and stored in mySchema
+// Customizes our empty class into a custom class and stored in mySchema.
 let messageSchema = new mongoose.Schema({
-    user: String,
-    message: String,
-    timestamp: Number
-//    let unixtime =   Math.round(new Date("2019").getTime()/1000);
-} );
+	user: String,
+	message: String,
+	timestamp: Number
+});
 
-// model lets us create a new database with the name messages and only allows the messageSchema
+// Model lets us create a new database with the name messages and only allows the messageSchema types in this database.
 let messageModel = new mongoose.model("messages", messageSchema);
 
-//Post handler
 
 
-      app.post("/saveMessage", (req,res)=> {
-    //    console.log(req.body.message);
+// Post handler
+app.post("/saveMessage", (req, res) => {
 
-    let date = new Date();  // classes
+	let date = new Date();
+	let allMessages;
+	
+	console.log(req.body.user);
 
+	let newMessage = new messageModel({
+		reallyCoolProp: "very important",
+		user: req.body.user,
+		message: req.body.message,
+		timestamp: date.getTime()
+	});
 
-    // build new object
-    let newMessage = new messageModel({
-        user: req.body.user,
-        message: req.body.message,
-        timestamp: date.getTime() //server's time
-    });
-
-    newMessage.save((error)=> {
-      if (error) {
-          console.log("there was an issue with mongoose",error);
-          res.sendStatus(500);
-        } else {
-           console.log("document saved");
-           res.sendStatus(200);
-        }
-    });
-
-
-
-});
-
-
-
-
-
-/*
-let usersSchema = new mongoose.Schema(
-{
-     // method to make row unique in database starts w/ underscore in mongoldb
-    "userid": String,
-    "firstName": String,
-    "lastName": String,
-    "age": Number,
-    "isActive": Boolean
-}
-);
-*/
-
-
-// to tell mongoose what collections to be saved in ... what to store where
-//let usersModel = new mongoose.model("users", usersSchema);
-// creating the first data for the mongol document
-/*
-let myFirstUser = new usersModel(
-{
-  userId: "TheGas",
-  "Name": "Jack",
-  age: 24,
-  isActive: true
-}
-);
-
-myFirstUser.save((error) => {
-
-  if (error) {
-    console.log(`There was an error saving your document! `);
-  } else {
-    console.log(`Your document was successfully saved to the database! `);
-  }
-
-
-
+	newMessage.save((error) => {
+		if (error) {
+			console.log("There was an issue with Mongoose", error);
+			res.sendStatus(500);
+		} else {
+			console.log("Document saved");
+			
+			
+			messageModel.find({}, (error, results) => {
+				console.log(results);	
+				allMessages = results;
+				
+				console.log(allMessages[3]._id);
+				
+				let objectToDelete = allMessages[3]._id;
+				
+				messageModel.findByIdAndDelete(objectToDelete, (error, results) => {
+					console.log(error, results);
+					console.log("The 4th item in the database was delete!");
+				});
+				
+				let objectToUpdate = allMessages[0]._id;
+				
+				messageModel.findByIdAndUpdate( objectToUpdate, {user: "Phone"}, (error, results) => {
+					console.log(error, results);
+					console.log("The 1 first item was updated.");			
+				});
+				
+				
+				
+			});
+			
+			
+			
+			
+			res.sendStatus(200);
+		}
+	});
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 
 });
-*/
-//
